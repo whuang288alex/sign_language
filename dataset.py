e00cb673992ccffc2763f2687152ee63d144a171
Alex Huang <whuang288@wisc.edu>
2022-11-08 21:01:22 -0600

    temp

--- dataset.py
+++ dataset.py
@@ -1,7 +1,12 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
 class CustomDataset(Dataset):
     
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
+    def __init__(self, annotations_file, img_dir, transform = None, target_transform = None):
+        self.csvfile = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
@@ -19,3 +24,5 @@ class CustomDataset(Dataset):
             label = self.target_transform(label)
         return image, label
     
+file = pd.read_csv("./sign_lang_mnist/sign_mnist_test.csv")
+print(file)
